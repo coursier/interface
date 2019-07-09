3ea83ee1750397121717a7aa438a6d0f0f852e2d
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-07-09 15:29:45 +0200

    Add coursierapi.Complete

--- interface/src/main/scala/coursier/internal/api/ApiHelper.scala
+++ interface/src/main/scala/coursier/internal/api/ApiHelper.scala
@@ -194,19 +194,9 @@ object ApiHelper {
       .withForcedProperties(params.getForcedProperties.asScala.iterator.toMap)
   }
 
-  def fetch(fetch: coursierapi.Fetch): Fetch[Task] = {
+  def cache(cache: coursierapi.Cache): FileCache[Task] = {
 
-    val dependencies = fetch
-      .getDependencies
-      .asScala
-      .map(dependency)
-
-    val repositories = fetch
-      .getRepositories
-      .asScala
-      .map(repository)
-
-    val loggerOpt = Option(fetch.getCache.getLogger).map[CacheLogger] {
+    val loggerOpt = Option(cache.getLogger).map[CacheLogger] {
       case s: SimpleLogger =>
         new CacheLogger {
           override def downloadingArtifact(url: String) =
@@ -222,10 +212,25 @@ object ApiHelper {
         w.getLogger
     }
 
-    val cache = FileCache()
-      .withPool(fetch.getCache.getPool)
-      .withLocation(fetch.getCache.getLocation)
+    FileCache()
+      .withPool(cache.getPool)
+      .withLocation(cache.getLocation)
       .withLogger(loggerOpt.getOrElse(CacheLogger.nop))
+  }
+
+  def fetch(fetch: coursierapi.Fetch): Fetch[Task] = {
+
+    val dependencies = fetch
+      .getDependencies
+      .asScala
+      .map(dependency)
+
+    val repositories = fetch
+      .getRepositories
+      .asScala
+      .map(repository)
+
+    val cache0 = cache(fetch.getCache)
 
     val classifiers = fetch
       .getClassifiers
@@ -239,7 +244,7 @@ object ApiHelper {
     var f = Fetch()
       .withDependencies(dependencies)
       .withRepositories(repositories)
-      .withCache(cache)
+      .withCache(cache0)
       .withMainArtifacts(fetch.getMainArtifacts)
       .withClassifiers(classifiers)
       .withFetchCache(Option(fetch.getFetchCache))
@@ -292,13 +297,35 @@ object ApiHelper {
       case Right(res) =>
         val l = new ju.ArrayList[ju.Map.Entry[coursierapi.Artifact, File]]
         for ((a, f) <- res.artifacts) {
-          val a0 = coursierapi.Artifact.of(a.url, a.changing, a.optional, ???)
+          val credentials0 = a.authentication.map(credentials).orNull
+          val a0 = coursierapi.Artifact.of(a.url, a.changing, a.optional, credentials0)
           val ent = new ju.AbstractMap.SimpleEntry(a0, f)
           l.add(ent)
         }
 
         coursierapi.FetchResult.of(l)
     }
+  }
+
+  def doComplete(complete: coursierapi.Complete): coursierapi.CompleteResult = {
+
+    val cache0 = cache(complete.getCache)
+
+    val repositories = complete
+      .getRepositories
+      .asScala
+      .map(repository)
+
+    val res = coursier.complete.Complete(cache0)
+      .withRepositories(repositories)
+      .withScalaBinaryVersion(Option(complete.getScalaBinaryVersion))
+      .withScalaVersion(Option(complete.getScalaVersion))
+      .withInput(complete.getInput)
+      .complete()
+      .unsafeRun()(cache0.ec)
+
+    val l = new ju.ArrayList[String](res._2.asJavaCollection)
+    coursierapi.CompleteResult.of(res._1, l)
   }
 
 }
