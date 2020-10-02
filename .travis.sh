7abc3bff676ae61311fb155ecb05c0d60eb1ede6
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-10-02 18:00:46 +0200

    Bump cs version on CI

--- .travis.sh
+++ .travis.sh
@@ -12,22 +12,10 @@ sbt \
 # test that things work from JDK 11
 # not actually building things from it, running into weird proguard issues…
 
-CS_VERSION="2.0.0-RC6-21"
-
-LAUNCHERS_DIR="$HOME/.cache/coursier/launchers"
-
-CS="$LAUNCHERS_DIR/$CS_VERSION/cs"
-if [ ! -x "$CS" ]; then
-  rm -rf "$LAUNCHERS_DIR" # remove any former launcher
-
-  DIR="$(dirname "$CS")"
-  mkdir -p "$DIR"
-  curl -Lo "$CS" "https://github.com/coursier/coursier/releases/download/v$CS_VERSION/cs-x86_64-pc-linux"
-  chmod +x "$CS"
-fi
+./.cs-setup.sh
 
 TEST_JDK="adopt:1.11.0-7"
-eval "$("$CS" java --jvm "$TEST_JDK" --env)"
+eval "$(./cs java --jvm "$TEST_JDK" --env)"
 
 java -Xmx32m -version
 
