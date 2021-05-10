99175e59f3657f9512a88371fbff3255bf40decc
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-05-10 17:41:36 +0200

    Make a class public to make javadoc happy

--- interface-svm-subs/src/main/java/coursierapi/shaded/scala/meta/internal/svm_subs/Target_scala_collection_immutable_VM.java
+++ interface-svm-subs/src/main/java/coursierapi/shaded/scala/meta/internal/svm_subs/Target_scala_collection_immutable_VM.java
@@ -5,7 +5,7 @@ import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 
 @TargetClass(className = "coursierapi.shaded.scala.collection.immutable.VM", onlyWith = HasReleaseFenceMethod.class)
-final class Target_scala_collection_immutable_VM {
+public final class Target_scala_collection_immutable_VM {
 
     @Substitute
     public static void releaseFence() {
