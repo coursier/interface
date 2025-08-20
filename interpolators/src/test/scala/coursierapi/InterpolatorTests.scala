023cbd2c850a624a39fd28da008d8372929f92e6
Alex Archambault <alexandre.archambault@gmail.com>
2025-08-20 22:37:07 +0200

    Stop using deprecated utest syntax

--- interpolators/src/test/scala/coursierapi/InterpolatorTests.scala
+++ interpolators/src/test/scala/coursierapi/InterpolatorTests.scala
@@ -6,33 +6,33 @@ import utest._
 object InterpolatorTests extends TestSuite {
 
   val tests = Tests {
-    'module - {
-      * - {
+    test("module") {
+      test {
         val mod = mod"some.org:a-module"
         val expectedMod = Module.of("some.org", "a-module")
         assert(mod == expectedMod)
       }
     }
 
-    'dependency - {
-      * - {
+    test("dependency") {
+      test {
         val dep = dep"some.org:a-module:0.1.2"
         val expectedDep = Dependency.of("some.org", "a-module", "0.1.2")
         assert(dep == expectedDep)
       }
     }
 
-    'repository - {
-      'maven - {
-        * - {
+    test("repository") {
+      test("maven") {
+        test {
           val repo = mvn"https://artifacts.com.pany/maven"
           val expectedRepo = MavenRepository.of("https://artifacts.com.pany/maven")
           assert(repo == expectedRepo)
         }
       }
 
-      'ivy - {
-        * - {
+      test("ivy") {
+        test {
           val repo = ivy"https://foo.com/a/b/c/[defaultPattern]"
           val expectedRepo = IvyRepository.of("https://foo.com/a/b/c/[defaultPattern]")
           assert(repo == expectedRepo)
